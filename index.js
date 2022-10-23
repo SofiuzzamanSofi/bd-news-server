f2d985ac56540bddd0c137150922b8222e2231a4
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2022-10-23 19:22:02 +0600

    news + news catagory + all news done

--- index.js
+++ index.js
@@ -6,15 +6,42 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
+
 
 app.get('/', (req, res) => {
-    res.send('6th time?, bd-news API running')
+    res.send('bd-news serverSide response send. success');
 });
+
 
 app.get('/news-categories', (req, res) => {
     res.send(categories);
+});
+
+app.get('/news', (req, res) => {
+    res.send(news);
 })
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news);
+    }
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+});
+
+
+
 
 app.listen(port, () => {
     console.log(`bd-news server running on port ${port}`);
